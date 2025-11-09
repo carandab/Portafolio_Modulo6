b4944471a2ae15c27437fedb093e5448386dd2b9
Cristian Aranda <c.aranda.borquez@gmail.com>
2025-11-08 21:58:07 -0300

    Se agregan templates para el projecto // Se agregan archivos static // Se modifica archivo settings para agregar statics y media // Se modifican urls

--- users/urls.py
+++ users/urls.py
@@ -5,7 +5,9 @@ from . import views
 app_name = 'users'
 
 urlpatterns = [
+
     path('register/', views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    
 ]
